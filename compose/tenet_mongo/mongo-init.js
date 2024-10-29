035be20a664015f6bfaf1c3c969be05a5e4a3f8f
Donatello Santoro <donatello.santoro@svelto.tech>
2024-10-29 14:59:40 +0100

    Fixed initdb script

--- compose/tenet_mongo/mongo-init.js
+++ compose/tenet_mongo/mongo-init.js
@@ -1,8 +1,10 @@
-use tenet
+db = db.getSiblingDB('tenet');
+
+db.createCollection('users');
 
 db.users.insertOne(
   {
     "username": "admin",
     "password": "0dc1e407b82b610ec695936497b694dd"
   }
-)
+);
